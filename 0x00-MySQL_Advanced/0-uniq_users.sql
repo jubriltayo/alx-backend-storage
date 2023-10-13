75fd6ef7ef82ea687503fffdd03fad4f25f76331
Tayo Jubril <jubriltayo@gmail.com>
2023-10-13 22:29:01 +0100

    Task 0 and 1

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,6 @@
+-- creates a table `users` which can be executed on any database
+CREATE TABLE IF NOT EXISTS users (
+		id INT AUTO_INCREMENT PRIMARY KEY,
+		email VARCHAR(255) NOT NULL UNIQUE,
+		name VARCHAR(255)
+);
---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,10 @@
+-- creates a table `users` that can be executed on any database
+
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE IF NOT EXISTS users (
+		id INT AUTO_INCREMENT PRIMARY KEY,
+		email VARCHAR(255) NOT NULL UNIQUE,
+		name VARCHAR(255),
+		country ENUM( 'US', 'CO', 'TN' ) DEFAULT 'US' NOT NULL
+);
