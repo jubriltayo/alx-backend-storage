3ad71ca9eb490f7173eac8e00d6aa42965478757
Tayo Jubril <jubriltayo@gmail.com>
2023-10-14 23:09:51 +0100

    Task 11

--- 0x00-MySQL_Advanced/11-need_meeting.sql
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -1,9 +1,9 @@
 -- SQL script that creates a view need_meeting that lists all students that have a score under 80 (strict) and no last_meeting or more than 1 month
 
-DROP VIEW IF EXITS need_meeting;
+DROP VIEW IF EXISTS need_meeting;
 
 CREATE VIEW need_meeting AS
-SELCECT name
+SELECT name
 FROM students
 WHERE score < 80
 		AND (
