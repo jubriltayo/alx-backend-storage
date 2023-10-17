40f9ec152d67c8aecd48731bbba98d82034cbb7a
Tayo Jubril <jubriltayo@gmail.com>
2023-10-18 00:37:43 +0100

    Tasks solution

---
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" Python function that returns the list of school having a specific topic """
+
+
+def schools_by_topic(mongo_collection, topic):
+    """ function that returns the list of school """
+    return list(mongo_collection.find({"topics": topic}))
