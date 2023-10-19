ef73a5afd3c87df415c82d9fde96ce53f5bf9612
Tayo Jubril <jubriltayo@gmail.com>
2023-10-19 04:13:34 +0100

    task 5

---
+++ 0x02-redis_basic/testfiles/4-test.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+import redis
+
+Cache = __import__('exercise').Cache
+replay = __import__('exercise').replay
+cache = Cache()
+cache.store("foo")
+cache.store("bar")
+cache.store(42)
+replay(cache.store)
